0c59322f2dea6ca8601cbaa48af4e37d70fca295
limdahee23 <limdahee23@ogqcorp.com>
2022-07-26 15:18:19 +0900

    add config

---
+++ sample-config/src/main/java/java/samplecontroller/SampleConfigApplication.java
@@ -0,0 +1,13 @@
+package java.samplecontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleConfigApplication.class, args);
+    }
+
+}
---
+++ sample-config/src/main/java/java/samplecontroller/Webconfig.java
@@ -0,0 +1,4 @@
+package java.samplecontroller;
+
+public class Webconfig {
+}
